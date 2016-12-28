d386010596b317a5792482b5c2c0d5446f028438
Ivan Lavriv <lavriv92@gmail.com>
2016-12-28 17:32:55 +0200

    Added modal window

---
+++ frontend/src/containers/ModalWindow/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ModalWindow extends Component {
+  render() {
+    return (
+      <section>
+        <section>
+          <i>close</i>
+        </section>
+      </section>
+    )
+  }
+}
+
+export default ModalWindow;
