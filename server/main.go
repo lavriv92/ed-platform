a530a59dfe7498cd80e09ec0b8df99e51a588c04
Ivan Lavriv <lavriv92@gmail.com>
2016-12-30 13:21:45 +0200

    API: Added router

--- server/main.go
+++ server/main.go
@@ -2,25 +2,84 @@ package main
 
 import (
     "fmt"
+    "html"
     "log"
     "time"
     "net/http"
     "encoding/json"
+    "github.com/gorilla/mux"
 )
 
 type Todo struct {
-  Name      string
-  Completed bool
-  Due       time.Time
+  Name      string    `json:"name"`
+  Completed bool      `json:"completed"`
+  Due       time.Time `json:"due"`
 }
 
 type Todos []Todo
 
-func main() {
-    http.HandleFunc("/", IndexHandler)
-    http.HandleFunc("/todos", TodosHandler)
+type Route struct {
+  Method      string
+  Pattern     string
+  Name        string
+  HandlerFunc http.HandlerFunc
+}
 
-    log.Fatal(http.ListenAndServe(":8888", nil))
+type Routes []Route
+
+func Logger(inner http.Handler, name string) http.Handler {
+  return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    inner.ServeHTTP(w, r)
+
+    log.Printf(
+        "%s\t%s\t%s\t%s",
+        r.Method,
+        r.RequestURI,
+        name,
+        time.Since(start),
+    )
+  })
+}
+
+func NewRouter() *mux.Router {
+  router := mux.NewRouter().StrictSlash(true)
+
+  for _, route := range routes {
+    var handler http.Handler
+    handler = route.HandlerFunc
+    handler = Logger(handler, route.Name)
+
+    router.
+      Methods(route.Method).
+      Path(route.Pattern).
+      Name(route.Name).
+      Handler(handler)
+  }
+  return router
+}
+
+var routes = Routes{
+  Route{
+    Method: "GET",
+    Pattern: "/todos",
+    Name: "todos",
+    HandlerFunc: TodosHandler,
+  },
+  Route{
+    Method: "GET",
+    Pattern: "/",
+    Name: "index",
+    HandlerFunc: IndexHandler,
+  },
+}
+
+func main() {
+
+    router := NewRouter()
+
+    log.Printf("Server listen on localhost:8888")
+    log.Fatal(http.ListenAndServe(":8888", router))
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,5 +92,5 @@ func TodosHandler(w http.ResponseWriter, r *http.Request) {
     Todo{Name: "Hello test todo"},
   }
 
-  json.newEncoder(w).Encode(todos)
+  json.NewEncoder(w).Encode(todos)
 }
