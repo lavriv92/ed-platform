15cab3c4e523ab6384e913885c8392aa8c6556bd
Ivan <lavriv92@gmail.com>
2017-02-03 00:46:26 +0200

    Add user tooltip and fake user avatar

--- src/components/ProfileMenu/index.js
+++ src/components/ProfileMenu/index.js
@@ -18,11 +18,14 @@ class ProfileMenu extends Component {
 
     return (
       <div className="profile-menu">
-        <span className="profile-link" onClick={this.toggleMenu}>Profile name</span>
+        <div className="profile-link" onClick={this.toggleMenu}>
+          <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/15032174_1260630360666891_8456893431431950772_n.jpg?oh=ca76772d53f7edb1eb087d0ddef96755&oe=59442597" alt="#" className="avatar" />
+          <small>First last</small>
+        </div>
         <menu className={`panel arrow-center-top ${visible ? 'visible' : ''}`}>
-          <div>My profile</div>
-          <div>Setting</div>
-          <div>Log out</div>
+          <div className="menu-item">My profile</div>
+          <div className="menu-item">Setting</div>
+          <div className="menu-item">Log out</div>
         </menu>
       </div>
     );
