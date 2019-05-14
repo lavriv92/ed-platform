147a8ef1a8619da7f0f6d41d0e9a056798445ebe
Ivan Lavriv <ivan.lavriv@perfectial.com>
2019-05-14 12:47:54 +0300

    Update repo

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,13 +11,13 @@ import Login from './components/Login';
 import Guide from './components/Guide';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Index}/>
-    <Route path="sign-up" component={SignUp} />
-    <Route path="classrooms" component={ClassRooms} />
-    <Route path="coaches" component={Coaches} />
-    <Route path="signin" component={Login} />
+    <Route path="/" component={App}>
+        <IndexRoute component={Index} />
+        <Route path="sign-up" component={SignUp} />
+        <Route path="classrooms" component={ClassRooms} />
+        <Route path="coaches" component={Coaches} />
+        <Route path="signin" component={Login} />
 
-    <Route path="guide" component={Guide} />
-  </Route>
+        <Route path="guide" component={Guide} />
+    </Route>
 );
